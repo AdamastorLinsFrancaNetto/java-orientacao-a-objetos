85c995cd9f4d407c0ea8accdc982915b613f3cf2
AdamastorLinsFrancaNetto <adamastorfranca@hotmail.com>
2021-06-13 03:58:24 -0300

    13/06/21

---
+++ ReservaAplicacao.java
@@ -0,0 +1,54 @@
+package aplicacao;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entidade.ReservaEntidade;
+import entidade.excecoes.ReservaExcecoes;
+
+public class ReservaAplicacao {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
+		
+		try {
+			System.out.print("Hóspede: ");
+			String hospede = sc.nextLine();
+			System.out.print("Nº do quarto: ");
+			int quarto = sc.nextInt();
+			System.out.print("Data de entrada (dd/MM/aaaa): ");
+			Date dataEntrada = dataFormatada.parse(sc.next());
+			System.out.print("Data de saida (dd/MM/aaaa): ");
+			Date dataSaida = dataFormatada.parse(sc.next());
+	
+			ReservaEntidade reserva1 = new ReservaEntidade
+			(hospede, quarto, dataEntrada, dataSaida);
+			System.out.println(reserva1);
+			
+			System.out.println("\nInforme a atualização para data de reserva:");
+			System.out.print("Data de entrada (dd/MM/aaaa): ");
+			dataEntrada = dataFormatada.parse(sc.next());
+			System.out.print("Data de saida (dd/MM/aaaa): ");
+			dataSaida = dataFormatada.parse(sc.next());
+			
+			reserva1.atualizarDatas(dataEntrada, dataSaida);
+			System.out.println(reserva1);
+		}
+		catch (ParseException e) {
+			System.out.println("Data inválida!");
+		}
+		catch (ReservaExcecoes e) {
+			System.out.println("Erro na reserva: " + e.getMessage());
+		}
+		catch (RuntimeException e) {
+			System.out.println("Erro inesperado!");
+		}
+	
+		sc.close();
+	}
+
+}
---
+++ ReservaEntidade.java
@@ -0,0 +1,81 @@
+package entidade;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import entidade.excecoes.ReservaExcecoes;
+
+public class ReservaEntidade {
+	
+	private String nomeCliente;
+	private Integer numeroQuarto;
+	private Date dataEntrada;
+	private Date dataSaida;
+	
+	private static SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public ReservaEntidade(String nomeCliente, Integer numeroQuartp, Date dataEntrada, Date dataSaida) throws ReservaExcecoes {
+		if (!dataSaida.after(dataEntrada)) {
+			throw new ReservaExcecoes("A data de saída não pode ser antes da data de entrada!");
+		}
+		this.nomeCliente = nomeCliente;
+		this.numeroQuarto = numeroQuartp;
+		this.dataEntrada = dataEntrada;
+		this.dataSaida = dataSaida;
+	}
+	
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public Integer getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+	public void setNumeroQuarto(Integer numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+
+	public Date getDataaEntrada() {
+		return dataEntrada;
+	}
+
+	public Date getDataSaida() {
+		return dataSaida;
+	}
+
+	public long duracao() {
+		long diferenca = dataSaida.getTime() - dataEntrada.getTime();
+		return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
+	}
+	
+	public void atualizarDatas(Date diaEntrada, Date diaSaida) throws ReservaExcecoes {
+		Date agora = new Date();
+		if (dataEntrada.before(agora) || dataSaida.before(agora)) {
+			throw new ReservaExcecoes("A data da reserva deve ser atualizada com datas futuras!");
+		}
+		if (dataEntrada.before(agora) && dataSaida.before(agora)) {
+			throw new ReservaExcecoes("A data da reserva deve ser atualizada com datas futuras!");
+		}
+		if (!dataSaida.after(dataEntrada)) {
+			throw new ReservaExcecoes("A data de saída não pode ser antes da data de entrada!");
+		}
+		this.dataEntrada = diaEntrada;
+		this.dataSaida = diaSaida;
+	}
+	
+	@Override
+	public String toString() {
+		return "\nRESERVA:"
+			+ "\nHóspede: " + getNomeCliente()
+			+ "\nNº do quarto: " + getNumeroQuarto()
+			+ "\nDia de Entrada: " + dataFormatada.format(dataEntrada)
+			+ "\nDia de Saída: " + dataFormatada.format(dataSaida)
+			+ "\nDuração: " + duracao() + " noites";
+	}
+}
